3dd2fdfa3364cc8f40f4a8e8b38a198acfe8816a
Richard Seroter <richardseroter@Richards-MacBook-Pro.local>
2019-11-12 10:59:50 -0800

    Added property value

--- src/main/java/com/seroter/demo/GreetingController.java
+++ src/main/java/com/seroter/demo/GreetingController.java
@@ -1,14 +1,18 @@
 package com.seroter.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 	
+	@Value("${appruntime:Spring Boot}")
+	private String appruntime;
+	
 	@GetMapping("/")
 	public String SayHi() {
-		return "Hello VMworld Europe! You're the best.";
+		return "Hello VMworld Europe! Greetings from " + appruntime;
 	}
 
 }
